bf1e5087e36b3ec47db7b3a754f2178695157fec
xmartina <martinsprince126@gmail.com>
2023-10-13 16:52:59 +0100

    fixed

--- depo/admin/admin_header.php
+++ depo/admin/admin_header.php
@@ -1,1 +1,30 @@
 <?php
+$rootDir = __DIR__;
+require_once ( "../depo/inc/config.php");
+session_start();
+
+// Check if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Get user input
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Query to check user credentials (change the table and column names)
+    $sql = "SELECT * FROM hm2_users WHERE username = '$username' AND password = '$password'";
+    $result = $conn->query($sql);
+
+    // Check if a matching user is found
+    if ($result->num_rows == 1) {
+        // User is authenticated; create a session
+        $_SESSION['username'] = $username;
+        header('Location: dashboard.php'); // Redirect to a dashboard page
+    } else {
+        // Authentication failed; display an error message
+        echo "Invalid username or password.";
+    }
+
+    // Close the database connection
+    $conn->close();
+}
+?>
--- depo/admin/login.php
+++ depo/admin/login.php
@@ -1,1 +1,20 @@
 <?php
+require_once ('admin_header.php');
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Admin Login From | Pending Deposit</title>
+    <link rel="stylesheet" href="assets/css/custom.css">
+</head>
+<body>
+    <div class="position-relative login_relative">
+        <div class="position-absolute login_absolute w-50"></div>
+    </div>
+</body>
+</html>
