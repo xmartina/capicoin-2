2fd5908416508c91513687702a038e9dec6cb57c
xmartina <martinsprince126@gmail.com>
2023-10-18 07:59:49 +0100

    fixed

--- depo/user/dashboard.php
+++ depo/user/dashboard.php
@@ -10,7 +10,6 @@
             <h4>
                 Note: only use this form if you are ready to make a deposit now
                 <?php echo $user_id; ?>
-                <?php echo $userID; ?>
             </h4>
             <form action="">
                 <div class="form-group input-group">
