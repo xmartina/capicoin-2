c4b7ff2c25977d425c87ab1958c8b4638478e2fe
xmartina <martinsprince126@gmail.com>
2023-10-18 09:31:46 +0100

    fixed

--- depo/user/footer.php
+++ depo/user/footer.php
@@ -2,14 +2,14 @@
 <script>
     var clipboard = new ClipboardJS('#copy-button');
 
-    clipboard.on('success', function(e) {
-        alert('Copied to clipboard: ' + e.text);
-        e.clearSelection();
-    });
-
-    clipboard.on('error', function(e) {
-        alert('Unable to copy. Please select and copy manually.');
-    });
+    // clipboard.on('success', function(e) {
+    //     alert('Copied to clipboard: ' + e.text);
+    //     e.clearSelection();
+    // });
+    //
+    // clipboard.on('error', function(e) {
+    //     alert('Unable to copy. Please select and copy manually.');
+    // });
 </script>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
