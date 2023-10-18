e57b2a39c8e21ac2794bc51e3f73fafd0c31bdc3
xmartina <martinsprince126@gmail.com>
2023-10-18 14:08:07 +0100

    fixed

--- depo/user/login.php
+++ depo/user/login.php
@@ -1,30 +1,42 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login Form</title>
+    <title>Auth Page</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="login-css.css">
 </head>
 <body>
 <h2>Login</h2>
-<form action="dashboard.php" method="post">
+<div class="container">
+    <div class="row">
+        <div class="col-lg-4"></div>
+        <div class="col-lg-4">
+            <form action="dashboard.php" method="post">
 
-    <div class="form-group">
-        <label for="inputEmail">Email or Username</label>
-        <input type="text" name="username_email" required class="form-control" id="inputEmail" placeholder="Email or Username">
+                <div class="form-group">
+                    <label for="inputEmail">Email or Username</label>
+                    <input type="text" name="username_email" required class="form-control" id="inputEmail" placeholder="Email or Username">
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword">Security Question:</label>
+                    <input type="text" name="security_question" required class="form-control" id="inputPassword" placeholder="Security Question">
+                </div>
+                <div class="form-group">
+                    <label for="inputPassword">Security Answer:</label>
+                    <input type="text" name="security_answer" required class="form-control" id="inputPassword" placeholder="Security Question">
+                </div>
+                <div class="form-group">
+                    <label class="form-check-label"><input type="checkbox"> Remember me</label>
+                </div>
+                <button type="submit" class="btn btn-primary" name="login" value="Login">Sign in</button>
+            </form>
+        </div>
     </div>
-    <div class="form-group">
-        <label for="inputPassword">Security Question:</label>
-        <input type="text" name="security_question" required class="form-control" id="inputPassword" placeholder="Security Question">
-    </div>
-    <div class="form-group">
-        <label for="inputPassword">Security Answer:</label>
-        <input type="text" name="security_answer" required class="form-control" id="inputPassword" placeholder="Security Question">
-    </div>
-    <div class="form-group">
-        <label class="form-check-label"><input type="checkbox"> Remember me</label>
-    </div>
-    <button type="submit" class="btn btn-primary" name="login" value="Login">Sign in</button>
-</form>
+</div>
 
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<!-- jQuery code to show/hide the second select based on the first select -->
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/js/bootstrap.min.js"></script>
 </body>
 </html>
