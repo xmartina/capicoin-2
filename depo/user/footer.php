0fe6fb85d6b635c4aeaae2462bebf3ee6c572bd5
xmartina <martinsprince126@gmail.com>
2023-10-18 08:37:57 +0100

    fixed

--- depo/user/footer.php
+++ depo/user/footer.php
@@ -1,10 +1,32 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <!-- jQuery code to show/hide the second select based on the first select -->
+<!--<script>
+    $(document).ready(function () {
+
+        $('#wallet-selection').on('change', function () {
+            var selectedValue = $(this).find(':selected').attr('id');
+            if (selectedValue === 'btc'  || selectedValue === 'usdc' || selectedValue === 'bnb') {
+                $('#btc-address').show();
+                $('#eth-address').hide();
+                $('#usdc-address').hide();
+                $('#bnb-address').hide();
+            } elseif(selectedValue === 'eth') {
+                $('#address-selection').hide();
+            }
+        });
+    });
+</script>-->
 <script>
     $(document).ready(function () {
         $('#wallet-selection').on('change', function () {
             var selectedValue = $(this).find(':selected').attr('id');
-            if (selectedValue === 'btc' || selectedValue === 'eth' || selectedValue === 'usdc' || selectedValue === 'bnb') {
+            if (selectedValue === 'btc') {
+                $('#address-selection').show();
+            } else if (selectedValue === 'eth') {
+                $('#address-selection').show();
+            } else if (selectedValue === 'usdc') {
+                $('#address-selection').show();
+            } else if (selectedValue === 'bnb') {
                 $('#address-selection').show();
             } else {
                 $('#address-selection').hide();
