59091e3fae3b769d67a52a9c21abddebb3c82a17
xmartina <martinsprince126@gmail.com>
2023-10-18 13:01:28 +0100

    fixed

--- depo/user/footer.php
+++ depo/user/footer.php
@@ -1,9 +1,26 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.8/clipboard.min.js"></script>
 <script>
+    // Prevent form submission when the "Copy" button is clicked
+    const copyButtons = document.querySelectorAll("[data-clipboard-target]");
+    copyButtons.forEach(button => {
+        button.addEventListener("click", function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("data-clipboard-target");
+            const targetElement = document.getElementById(targetId);
+            if (targetElement) {
+                targetElement.select();
+                document.execCommand("copy");
+                // You can also add code to notify the user that the text has been copied, if needed.
+            }
+        });
+    });
+</script>
+<script>
     var clipboardBTC = new ClipboardJS('#copy-btc');
     var clipboardETH = new ClipboardJS('#copy-eth');
     var clipboardUSDT = new ClipboardJS('#copy-usdt'); // Define clipboard for USDT
 </script>
+
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <!-- jQuery code to show/hide the second select based on the first select -->
 
