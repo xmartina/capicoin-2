087bf13a327a9fe21708a34d7305947bb2edecd7
xmartina <martinsprince126@gmail.com>
2023-10-18 09:17:07 +0100

    fixed

--- depo/user/dashboard.php
+++ depo/user/dashboard.php
@@ -43,9 +43,9 @@
                         Use the wallet address below to make your deposit
                     </div>
                     <div class="form-group input-group">
-                        <div class="row align-items-center">
+                        <div class="row align-items-center w-100">
                             <div class="col-sm-4 pr-0">BTC</div>
-                            <div class="col-sm-8 pl-2">
+                            <div class="col-sm-8 ">
                                 <input disabled name="username w-100" class="form-control w-100"
                                        value="bc1qldk0yfuj66hs6406murakdewqs9e6drjezzdd0"
                                        placeholder="Full name" type="text">
