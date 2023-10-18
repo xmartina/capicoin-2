34b9c8680062931e3dec1ae90eb4f6a08ae76f11
xmartina <martinsprince126@gmail.com>
2023-10-18 13:18:37 +0100

    fixed

---
+++ depo/user/deposit-stat.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Deposit Stat</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4"></div>
+            <div class="col-lg-4">
+                <div class="row">
+                    <div class="col-sm-2 rounded-circle bg-success text-white p-3">
+                        <span class="material-symbols-outlined w-100">done</span>
+                    </div>
+                    <div class="col-sm-10">
+                        <span class="depo-stat-hd">
+                        Your deposit was has been submitted successfully
+                        </span>
+                    </div>
+                    <div class="col-sm-2"></div>
+                    <div class="col-sm-10">
+                        <span class="depo-stat-bd">
+                            Remember to make your deposit using the wallet addresses provided for you in the previous page for your deposit to be approved
+                        </span>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</body>
+</html>
