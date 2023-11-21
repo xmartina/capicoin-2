210c6d753ae05d118d215297836e15148f9a5e07
xmartina <martinsprince126@gmail.com>
2023-11-21 06:48:03 +0100

    c

--- depo/user/login.php
+++ depo/user/login.php
@@ -34,7 +34,7 @@
             <div class="py-3"></div>
         </div>
 
-        <div class="py-5">
+        <div class="py-5 ww d-flex align-items-center justify-content-center">
             <div class="fixed-con">
                 <div id='google_translate_element'></div>
             </div>
