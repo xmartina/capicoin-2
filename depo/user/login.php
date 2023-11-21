145dceff7ffe6055d0c80fee74b0c4577875661e
xmartina <martinsprince126@gmail.com>
2023-11-21 06:51:50 +0100

    c

--- depo/user/login.php
+++ depo/user/login.php
@@ -44,6 +44,8 @@
 </div>
 
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="/front_assets/js/bundle.js?ver=3.2.0"></script>
+<script src="/front_assets/js/scripts.js?ver=3.2.0"></script>
 <!-- jQuery code to show/hide the second select based on the first select -->
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/4.6.2/js/bootstrap.min.js"></script>
